32711701185c6a5fae23b2830dd95975f4ae9635
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2023-09-11 02:52:37 -0300

    Update dark.ts

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -1,11 +1,11 @@
-export default {
+const d = {
     title: 'dark',
-
     colors: {
-        primary:'#000',
-        secundary: '#D63AF9',
+        primary: '#000',
+        secondary: '#D63AF9',
         background: '#000',
         text: '#fff'
     },
-
 };
+
+export default d;
