9ed23eca7e7b3e70abae3a9e7c2a9bf09a1db06b
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2023-09-11 02:52:56 -0300

    Update light.ts

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,11 +1,11 @@
-export default {
+const l = {
     title: 'light',
-
     colors: {
-        primary:'#6B6B6B',
-        secundary: '#D63AF9',
+        primary: '#6B6B6B',
+        secondary: '#D63AF9',
         background: '#6B6B6B',
         text: '#333'
     },
-
 };
+
+export default l;
